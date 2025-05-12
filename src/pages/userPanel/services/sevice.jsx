79065d10e14e34e99ff48a92836971f015607986
Gurpreet-kaur-786 <gurpreetguri0139@gmail.com>
2025-05-12 14:06:58 +0530

    Update sevice.jsx

--- src/pages/userPanel/services/sevice.jsx
+++ src/pages/userPanel/services/sevice.jsx
@@ -7,9 +7,9 @@ function Service() {
         <div className='ServiceBox'>
             
             <div className='ServiceBox2'>
-            <CardComponent img={'public/Images/card4.jpg'} heading={'Our History'} para={'The secret to happiness lies in helping others. Never underestimate the difference YOU can make in the lives of the poor.'}/>
+            <CardComponent img={'/Images/card4.jpg'} heading={'Our History'} para={'The secret to happiness lies in helping others. Never underestimate the difference YOU can make in the lives of the poor.'}/>
 
-            <CardComponent img={'public/Images/card5.jpg'} heading={'Our Mission'} para={'The secret to happiness lies in helping others. Never underestimate the difference YOU can make in the lives of the poor.'}/>
+            <CardComponent img={'/Images/card5.jpg'} heading={'Our Mission'} para={'The secret to happiness lies in helping others. Never underestimate the difference YOU can make in the lives of the poor.'}/>
             </div>
 
 
