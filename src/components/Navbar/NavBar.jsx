2bacb93f10dfe26dddfd234e3a7b8422d681c6ec
Gurpreet-kaur-786 <gurpreetguri0139@gmail.com>
2025-05-12 14:09:44 +0530

    Update NavBar.jsx

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -18,7 +18,7 @@ function Navbar() {
         <>
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
                 <div class="container-fluid">
-                <img src='/public/images/logo-white.svg' alt='' />
+                <img src='/images/logo-white.svg' alt='' />
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
