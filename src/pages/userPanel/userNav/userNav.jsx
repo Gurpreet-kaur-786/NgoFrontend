a0b89e06d8e76e2400144cb2c541cf558cb264bf
Gurpreet-kaur-786 <gurpreetguri0139@gmail.com>
2025-06-12 11:12:45 +0530

    Update userNav.jsx

--- src/pages/userPanel/userNav/userNav.jsx
+++ src/pages/userPanel/userNav/userNav.jsx
@@ -20,7 +20,7 @@ function UserNav() {
                     <div class="container-fluid">
                         <a class="navbar-brand" href="#">
 
-                            <img src='/images/logo-white.svg' alt='' />
+                            <img src='/Images/logo-white.svg' alt='' />
                         </a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
